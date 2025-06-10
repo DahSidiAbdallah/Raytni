ddaeb3ea7e4a0e6520351e58eaf448f42fadf882
Dah Sidi Abdallah <61889392+DahSidiAbdallah@users.noreply.github.com>
2025-06-10 13:51:44 +0000

    Implement Firebase Database for Signalements

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,23 +1,20 @@
 // src/lib/firebase.ts
 import { initializeApp } from "firebase/app";
-// import { getAuth } from "firebase/auth"; // Removed
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyC2omn-M8ImhB1-6I3aJ-a-sl_3YfDTUxo",
-    authDomain: "raytni-504cf.firebaseapp.com",
-    projectId: "raytni-504cf",
-    storageBucket: "raytni-504cf.firebasestorage.app",
-    messagingSenderId: "430221151219",
-    appId: "1:430221151219:web:5a667d03eb396ed9d76086",
-    measurementId: "G-PC17ZLG9RK"
-  };
+  apiKey: "AIzaSyC2omn-M8ImhB1-6I3aJ-a-sl_3YfDTUxo",
+  authDomain: "raytni-504cf.firebaseapp.com",
+  projectId: "raytni-504cf",
+  storageBucket: "raytni-504cf.appspot.com",
+  messagingSenderId: "430221151219",
+  appId: "1:430221151219:web:5a667d03eb396ed9d76086",
+  measurementId: "G-PC17ZLG9RK"
+};
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// export const auth = getAuth(app); // Removed
 export const db = getFirestore(app);
 export const storage = getStorage(app);
